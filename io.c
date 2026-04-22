224e18aa42952b01a86e1752b87908feddff88d6
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-22 15:00:15 +0100

    Update io.c

--- io.c
+++ io.c
@@ -3,21 +3,18 @@
 #include "io.h"
 
 int load_csv(const char *filename, WaveformSample *dataset, int row_count) {
-    FILE *file = fopen(filename, "r"); // Open file for reading
+    FILE *file = fopen(filename, "r"); 
 
     if (file == NULL) {
-        return 0; // Return 0 to signal "File Not Found"
+        return 0; 
     }
 
-    char buffer[1024]; // A temporary "bucket" to hold one line of text
+    char buffer[1024]; 
 
-    // 1. Skip the header row
     fgets(buffer, sizeof(buffer), file);
 
-    // 2. Loop through the rows [cite: 249, 250]
     for (int i = 0; i < row_count; i++) {
         if (fgets(buffer, sizeof(buffer), file)) {
-            // sscanf looks for 8 doubles (%lf) separated by commas
             sscanf(buffer, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",
                    &dataset[i].timestamp,
                    &dataset[i].phase_A_voltage,
