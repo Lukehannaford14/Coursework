415c9b5a3a040dbd33ccb901f29fcc1c81a59961
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-22 15:04:44 +0100

    Update io.h

--- io.h
+++ io.h
@@ -3,8 +3,7 @@
 
 #include "waveform.h"
 
-// Function prototype: takes the filename and the pointer to your memory
 int load_csv(const char *filename, WaveformSample *dataset, int row_count);
 
 
-#endif //UNTITLED_IO_H
+#endif 
