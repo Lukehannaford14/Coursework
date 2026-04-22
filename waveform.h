a207f47a3ca4406b9c869d6f540e92e056a72354
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-22 15:04:11 +0100

    Update waveform.h
    Added initial pointers for rms, offset and clipped calculations

--- waveform.h
+++ waveform.h
@@ -8,10 +8,14 @@ typedef struct {
     double phase_A_voltage;
     double phase_B_voltage;
     double phase_C_voltage;
+    double frequency;
     double line_current;
     double power_factor;
     double thd_percent;
 } WaveformSample;
 
+double compute_rms(WaveformSample *ptr, int n);
+double compute_dc_offset(WaveformSample *ptr, int n);
+int count_clipped_samples(WaveformSample *ptr, int n);
 
 #endif
