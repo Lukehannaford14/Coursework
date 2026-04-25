551e09b6f6bd4af88a207a8cac9d2721b51e2c98
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-25 16:14:36 +0100

    Update Main.c
    Added RMS, P2P and DC Offset

--- Main.c
+++ Main.c
@@ -16,10 +16,29 @@ int main(int argc, char *argv[]) {
     if (dataset == NULL) return 1;
 
     if (load_csv(argv[1], dataset, row_count)) {
-        printf("Data loaded successfully from %s\n", argv[1]);
+        printf("Data loaded successfully from %s\n\n", argv[1]);
 
-        printf("First Phase A Voltage: %.2f V\n", dataset[0].phase_A_voltage);
-    } else {
+        //RMS Calc
+        double phase_A_rms = calculate_phase_A_rms(dataset, row_count);
+
+        printf("Phase A RMS Voltage: %.2f V\n", phase_A_rms);
+
+        if (phase_A_rms >= 207.0 && phase_A_rms <= 253.0) {
+            printf("Status: COMPLIANT\n");
+        } else {
+            printf("Status: WARNING - OUT OF TOLERANCE\n");
+        }
+
+        //Peak To Peak
+        double phase_A_p2p = calculate_phase_A_peak_to_peak(dataset, row_count);
+        printf("Phase A Peak-to-Peak Voltage: %.2f V\n", phase_A_p2p);
+
+        //DC Offset
+        double phase_A_dc = calculate_phase_A_dc_offset(dataset, row_count);
+        printf("Phase A DC Offset: %.2f V\n", phase_A_dc);
+
+    }
+    else {
         printf("Error: Could not open file %s\n", argv[1]);
     }
 
