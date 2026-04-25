9e5cf94ece548aab7d5c0732a06c76779fe9a267
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-25 17:48:27 +0100

    Update waveform.h

--- waveform.h
+++ waveform.h
@@ -15,13 +15,31 @@ typedef struct {
 } WaveformSample;
 
 
-// Phase A RMS
+// Phase A   ----------------------------------------------------
 double calculate_phase_A_rms(WaveformSample *data, int num_samples);
 
-// Phase A P2P
 double calculate_phase_A_peak_to_peak(WaveformSample *dataset, int row_count);
 
-// Phase A DC Offset
 double calculate_phase_A_dc_offset(WaveformSample *dataset, int row_count);
+
+int count_phase_A_clipping(WaveformSample *dataset, int row_count);
+
+// Phase B   ----------------------------------------------------
+double calculate_phase_B_rms(WaveformSample *dataset, int row_count);
+
+double calculate_phase_B_peak_to_peak(WaveformSample *dataset, int row_count);
+
+double calculate_phase_B_dc_offset(WaveformSample *dataset, int row_count);
+
+int count_phase_B_clipping(WaveformSample *dataset, int row_count);
+
+// Phase C   ----------------------------------------------------
+double calculate_phase_C_rms(WaveformSample *dataset, int row_count);
+
+double calculate_phase_C_peak_to_peak(WaveformSample *dataset, int row_count);
+
+double calculate_phase_C_dc_offset(WaveformSample *dataset, int row_count);
+
+int count_phase_C_clipping(WaveformSample *dataset, int row_count);
 
 #endif
