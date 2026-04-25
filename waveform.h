a4df782cb25b03ce9409c9262e6619aeef8f5882
Lukehannaford14 <luke2.hannaford@live.uwe.ac.uk>
2026-04-25 16:15:48 +0100

    Update waveform.h

--- waveform.h
+++ waveform.h
@@ -1,21 +1,27 @@
 #ifndef UNTITLED_WAVEFORM_H
 #define UNTITLED_WAVEFORM_H
-
 #include <stdint.h>
 
+// Waveform Sample
 typedef struct {
     double timestamp;
     double phase_A_voltage;
     double phase_B_voltage;
     double phase_C_voltage;
-    double frequency;
     double line_current;
+    double frequency;
     double power_factor;
     double thd_percent;
 } WaveformSample;
 
-double compute_rms(WaveformSample *ptr, int n);
-double compute_dc_offset(WaveformSample *ptr, int n);
-int count_clipped_samples(WaveformSample *ptr, int n);
+
+// Phase A RMS
+double calculate_phase_A_rms(WaveformSample *data, int num_samples);
+
+// Phase A P2P
+double calculate_phase_A_peak_to_peak(WaveformSample *dataset, int row_count);
+
+// Phase A DC Offset
+double calculate_phase_A_dc_offset(WaveformSample *dataset, int row_count);
 
 #endif
